af7b675b1705e3b795421674b96582a5e2908707
unknown <nicholas_fernandes2017@hotmail.com>
2022-05-03 23:56:21 -0300

    commit inicial

---
+++ src/main/java/com/projeto/rolezin/RolezinApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.rolezin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolezinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolezinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/rolezin/RolezinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.rolezin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolezinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
