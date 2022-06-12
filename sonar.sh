8b8fe02e58ed88cd03527e65767ebdf0bdd01708
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-06-12 18:44:24 -0300

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,1 +1,2 @@
-./gradlew -Dsonar.projectKey=ufba-poo-2022-1_equipe2 -Dsonar.organization=ufba-poo-2022-1 -Dsonar.host.url=https://sonarcloud.io
+#./gradlew -Dsonar.projectKey=ufba-poo-2022-1_equipe2 -Dsonar.organization=ufba-poo-2022-1 -Dsonar.host.url=https://sonarcloud.io sonarqube
+./gradlew sonarqube
