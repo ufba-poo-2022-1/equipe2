d4f1c96866be080b5852dc252b942d18ab355722
unknown <nicholas_fernandes2017@hotmail.com>
2022-06-26 16:40:22 -0300

    roda ae pls

--- src/main/java/com/projeto/rolezin/eventos/repository/EventosRepository.java
+++ src/main/java/com/projeto/rolezin/eventos/repository/EventosRepository.java
@@ -3,9 +3,11 @@ package com.projeto.rolezin.eventos.repository;
 import com.projeto.rolezin.eventos.model.EventosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EventosRepository extends JpaRepository<EventosModel, Long> {
 
     @Query("select e from EventosModel e where e.nome like concat('%', ?1, '%')")
--- src/main/java/com/projeto/rolezin/usuários/repository/UsuariosRepository.java
+++ src/main/java/com/projeto/rolezin/usuários/repository/UsuariosRepository.java
@@ -3,7 +3,9 @@ package com.projeto.rolezin.usuários.repository;
 import com.projeto.rolezin.usuários.model.UsuariosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuariosRepository extends JpaRepository<UsuariosModel,Long> {
 
 
