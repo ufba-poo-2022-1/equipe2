de231be562884e0dda7f1b4eb8665205e2191d51
unknown <nicholas_fernandes2017@hotmail.com>
2022-06-26 18:48:35 -0300

    fixed cors

--- src/main/java/com/projeto/rolezin/atracoes/controller/AtracoesController.java
+++ src/main/java/com/projeto/rolezin/atracoes/controller/AtracoesController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+@CrossOrigin
 @RestController
 @RequestMapping("/eventos/atracoes")
 public class AtracoesController {
--- src/main/java/com/projeto/rolezin/config/ConfigurationCors.java
+++ src/main/java/com/projeto/rolezin/config/ConfigurationCors.java
@@ -8,6 +8,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+
 @Configuration
 public class ConfigurationCors {
 
